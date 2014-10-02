a86aa05f19972340b824d66a6250c865fcca910d
Ramon Wijnands <rayman747@hotmail.com>
2014-10-02 13:12:10 +0200

    show the ros connection status

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,8 +2,43 @@
 
 var app = angular.module('app', []);
 
-app.controller('MainCtrl', ['$scope', function ($scope) {
+app.controller('MainCtrl', ['$scope', 'ros', function ($scope, ros) {
   $scope.title = '<%= appName %>';
-  $scope.greeting = 'Hola!';
+
   $scope.status = 'btn-primary';
+
+  ros.on('connection', function () {
+    $scope.rosStatus = 'connection';
+  });
+
+  ros.on('error', function () {
+    $scope.rosStatus = 'error';
+  });
+
+  ros.on('close', function () {
+    $scope.rosStatus = 'close';
+  });
+
+
 }]);
+
+app.factory('ros', function ($rootScope) {
+  var hostname = window.location.hostname || 'localhost';
+  var rosUrl = 'ws://' + hostname + ':9090';
+
+  var ros = new ROSLIB.Ros({
+    url: rosUrl,
+  });
+
+  return {
+    ros: ros,
+    on: function (eventName, callback) {
+      ros.addListener(eventName, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(ros, args);
+        });
+      });
+    }
+  };
+});
