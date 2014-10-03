34c4d2747e3d8470089a59e797fed54b4d3ac6e4
Ramon Wijnands <rayman747@hotmail.com>
2014-10-03 15:46:36 +0200

    implement pinging

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -21,6 +21,17 @@ app.controller('MainCtrl', function ($scope, ros, Ping) {
   $scope.$on('ros:close', function () {
     $scope.rosStatus = 'close';
   });
+
+  var ping = new Ping(ros);
+  ping.forward($scope);
+
+  $scope.$on('ping:ok', function () {
+    $scope.rosStatus = 'ok';
+  });
+
+  $scope.$on('ping:timeout', function () {
+    $scope.rosStatus = 'timeout';
+  });
 });
 
 app.factory('ros', function ($rootScope, $timeout) {
@@ -75,6 +86,70 @@ app.factory('ros', function ($rootScope, $timeout) {
   };
 });
 
-app.factory('Ping', function (ros) {
+app.factory('Ping', function ($rootScope) {
 
+  var pingInterval = 5000; // ms. The time between pings
+  var pingTimeout  = 2000; // ms. If ros doesn't respond within this period of time, close the connection
+
+  var defaultScope = $rootScope;
+  // when forwarding events, prefix the event name
+  var prefix = 'ping:';
+
+  var Ping = function (ros) {
+    this.ros = ros.ros;
+    this.timeoutEventName = prefix + 'timeout';
+    this.okEventName      = prefix + 'ok';
+  };
+
+  Ping.prototype.forward = function (scope) {
+    var that = this;
+    if (!scope) {
+      scope = defaultScope;
+    }
+
+    // initialize the ping service to node_alive
+    var pingClient = new ROSLIB.Service({
+      ros : that.ros,
+      name : '/get_alive_nodes',
+      serviceType : 'node_alive/ListNodesAlive'
+    });
+
+    // this function gets called every pingInterval seconds
+    var pingNodesAlive = function () {
+      var request = new ROSLIB.ServiceRequest({});
+      var start = new Date();
+
+      setTimeout(function() {
+        if (start !== -1) { // check if already received a response
+          console.log(that.timeoutEventName, pingTimeout);
+          scope.$broadcast(that.timeoutEventName, pingTimeout);
+        }
+      }, pingTimeout);
+
+      pingClient.callService(request, function(result) {
+        var diff = new Date() - start;
+        start = -1;
+
+        console.log(that.okEventName, diff);
+        scope.$broadcast(that.okEventName, diff);
+      });
+    };
+
+    var pingId;
+    // start ping after the ros connection is started
+    that.ros.addListener('connection', function() {
+      setTimeout(that.pingNodesAlive, pingTimeout);
+      pingId = setInterval(pingNodesAlive, pingInterval);
+    });
+    // and stop when the connection is closed
+    that.ros.addListener('close', function() {
+      clearInterval(pingId);
+    });
+
+    scope.$on('$destroy', function () {
+      // clear the interval
+    });
+  };
+
+  return Ping;
 });
