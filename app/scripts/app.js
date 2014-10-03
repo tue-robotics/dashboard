5da6ed5f41dfe7cb209ce5adb68515531b35ae17
Ramon Wijnands <rayman747@hotmail.com>
2014-10-03 14:58:11 +0200

    implement ros.forward to forward ros events to angular

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,33 +2,47 @@
 
 var app = angular.module('app', []);
 
-app.controller('MainCtrl', ['$scope', 'ros', function ($scope, ros) {
+app.controller('MainCtrl', function ($scope, ros, Ping) {
   $scope.title = '<%= appName %>';
 
   $scope.status = 'btn-primary';
 
-  ros.on('connection', function () {
-    $scope.rosStatus = 'connection';
-  });
+  // forward all ros events to the current scope
+  ros.forward(['connection', 'error', 'close'], $scope);
 
-  ros.on('error', function () {
+  $scope.$on('ros:connection', function (ev, data) {
+    $scope.rosStatus = 'connection';
+  })
+
+  $scope.$on('ros:error', function () {
     $scope.rosStatus = 'error';
   });
 
-  ros.on('close', function () {
+  $scope.$on('ros:close', function () {
     $scope.rosStatus = 'close';
   });
+});
 
+app.factory('ros', function ($rootScope, $timeout) {
+  var defaultScope = $rootScope;
+  // when forwarding events, prefix the event name
+  var prefix = 'ros:';
 
-}]);
-
-app.factory('ros', function ($rootScope) {
   var hostname = window.location.hostname || 'localhost';
   var rosUrl = 'ws://' + hostname + ':9090';
 
   var ros = new ROSLIB.Ros({
     url: rosUrl,
   });
+
+  var asyncAngularify = function (that, callback) {
+    return callback ? function () {
+      var args = arguments;
+      $timeout(function () {
+        callback.apply(that, args);
+      }, 0);
+    } : angular.noop;
+  };
 
   return {
     ros: ros,
@@ -39,6 +53,28 @@ app.factory('ros', function ($rootScope) {
           callback.apply(ros, args);
         });
       });
+    },
+    forward: function (events, scope) {
+      if (!Array.isArray(events)) {
+        events = [events];
+      }
+      if (!scope) {
+        scope = defaultScope;
+      }
+      events.forEach(function (eventName) {
+        var prefixedEvent = prefix + eventName;
+        var forwardBroadcast = asyncAngularify(ros, function (data) {
+          scope.$broadcast(prefixedEvent, data);
+        });
+        scope.$on('$destroy', function () {
+          ros.removeListener(eventName, forwardBroadcast);
+        });
+        ros.on(eventName, forwardBroadcast);
+      });
     }
   };
+});
+
+app.factory('Ping', function (ros) {
+
 });
