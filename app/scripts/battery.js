997162772614e0182198fb8858ac3b0fc9891b78
Ramon Wijnands <rayman747@hotmail.com>
2014-10-17 16:30:50 +0200

    implement a battery controller

---
+++ app/scripts/battery.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var app = angular.module('app');
+
+app.controller('BatteryCtrl', function ($scope, ros) {
+
+  var listener = new ROSLIB.Topic({
+    ros : ros.ros,
+    name : '/amigo/battery_percentage',
+    messageType : 'std_msgs/Float32'
+  });
+
+  listener.subscribe(function(message) {
+    var percent = message.data; // float32
+    set_battery(percent);
+    console.log('Received message on ' + listener.name + ': ' + message.data);
+  });
+
+  function set_battery(percent) {
+    $scope.$apply(function () {
+      $scope.battery = percent;
+    })
+  }
+
+  $scope.$watch('battery', function(){
+    var type, value = $scope.battery;
+    if (value > 40) {
+      type = 'success';
+    } else if (value > 20) {
+      type = 'warning';
+    } else {
+      type = 'danger';
+    }
+    $scope.batteryType = type;
+  });
+});
