4787b026b650085c7fcf76e5c2ff408f63b0c1fd
Ramon Wijnands <rayman747@hotmail.com>
2014-10-28 21:38:00 +0100

    implement the emergency buttons

--- app/scripts/runstop.js
+++ app/scripts/runstop.js
@@ -6,12 +6,52 @@ angular.module('app')
   return {
     restrict: 'E',
     transclude: true,
-    template: '<button class="btn btn-xs btn-circle" ng-transclude></button>',
+    template: '<button class="btn btn-xs btn-circle" ng-class="class" ng-transclude></button>',
 
     scope: {
+      type: '=',
     },
-    controller: function ($scope, ros) {
-      console.log('in the controller');
+    controller: function ($scope, $attrs, ros) {
+      var topic;
+      if ($attrs.type == 'wireless') {
+        topic = '/amigo/emergency_switch';
+      } else {
+        topic = '/amigo/runstop';
+      }
+
+      var inTopic = new ROSLIB.Topic({
+        ros: ros.ros,
+        name: topic,
+        messageType: 'std_msgs/Bool',
+        throttle_rate: 2,
+      });
+
+      function setEnabled (enabled) {
+        $scope.$apply(function () {
+          $scope.enabled = enabled;
+        });
+      }
+
+      var unsetEnabled = _.debounce(function () {
+        console.log(unsetEnabled);
+        setEnabled(undefined);
+      }, 2000);
+
+      inTopic.subscribe(function(message) {
+        setEnabled(message.data);
+        unsetEnabled();
+      });
+
+      $scope.$watch('enabled', function(enabled) {
+        console.log(enabled);
+        if (typeof enabled == 'undefined') {
+          $scope.class = 'btn-default';
+        } else if (enabled) {
+          $scope.class = 'btn-danger';
+        } else {
+          $scope.class = 'btn-success';
+        }
+      });
     }
   };
 });
