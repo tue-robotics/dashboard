422e780143971eb5bd4cfb056829129b2af41dce
Ramon Wijnands <rayman747@hotmail.com>
2014-12-16 20:27:22 +0100

    add extra comments for the ros status

--- app/scripts/status-controller.js
+++ app/scripts/status-controller.js
@@ -2,6 +2,13 @@
 
 var app = angular.module('app');
 
+/**
+ * Status overview:
+ * - connecting - will listen for hardware messages to go to ok
+ * - ok         - 
+ * - error      - 
+ * - closed     - in this state, we will try to reconnect each 5 secs
+ */
 app.controller('StatusCtrl', function ($scope, ros, Hardware) {
 
   $scope.title = ros.rosbridge_url;
