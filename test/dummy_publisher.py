2d5554d485c4edfe5cde6c5dc5f4a0846b307ed2
Ramon Wijnands <rayman747@hotmail.com>
2015-01-06 21:48:21 +0100

    add HardwarePublisher dummy

--- test/dummy_publisher.py
+++ test/dummy_publisher.py
@@ -12,7 +12,7 @@ class DiagnosticArrayPublisher():
         self.pub = rospy.Publisher(self.topic, DiagnosticArray, queue_size=10)
     def send(self):
 
-        self.pub.publish()
+        self.pub.publish(self.generate_msg())
 
 class EbuttonPublisher(DiagnosticArrayPublisher):
     topic = 'amigo/ebutton_status'
@@ -25,20 +25,54 @@ class EbuttonPublisher(DiagnosticArrayPublisher):
             DiagnosticStatus(
                 level=1,
                 name="Wireless"
-            )
+            ),
         ]
         return DiagnosticArray(status=status)
+
+class HardwarePublisher(DiagnosticArrayPublisher):
+    topic = 'amigo/hardware_status'
+    def generate_msg(self):
+        status = [
+            DiagnosticStatus(
+                level=0,
+                name="all"
+            ),
+            DiagnosticStatus(
+                level=1,
+                name="base"
+            ),
+            DiagnosticStatus(
+                level=2,
+                name="spindle"
+            ),
+            DiagnosticStatus(
+                level=3,
+                name="left_arm"
+            ),
+            DiagnosticStatus(
+                level=4,
+                name="right_arm"
+            ),
+            DiagnosticStatus(
+                level=1,
+                name="head"
+            ),
+        ]
+        return DiagnosticArray(status=status)
+
 
 if __name__ == '__main__':
     rospy.init_node('dummy_publisher', anonymous=True)
 
     try:
         epub = EbuttonPublisher()
+        hpub = HardwarePublisher()
 
         rospy.loginfo('publishing...')
         rate = rospy.Rate(10)
         while not rospy.is_shutdown():
             epub.send()
+            hpub.send()
             rate.sleep()
     except rospy.ROSInterruptException:
         pass
