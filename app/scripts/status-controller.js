d3c6f94d388d521759d7dfbccb7d67bc54b6f9e0
Ramon Wijnands <rayman747@hotmail.com>
2015-09-22 17:18:43 +0200

    let status use the robot-api

--- app/scripts/status-controller.js
+++ app/scripts/status-controller.js
@@ -5,43 +5,16 @@ var app = angular.module('app');
 /**
  * Status overview:
  * - connecting - will listen for hardware messages to go to ok
- * - ok         - 
- * - error      - 
+ * - ok         -
+ * - error      -
  * - closed     - in this state, we will try to reconnect each 5 secs
  */
-app.controller('StatusCtrl', function ($scope, ros, Hardware) {
+app.controller('StatusCtrl', function ($scope, robot) {
 
-  $scope.title = ros.rosbridge_url;
+  $scope.title = robot.ros.socket.url;
 
-  // if no message arrived for 2000 ms,
-  // set the ros status back to 'connecting'
-  var checkConnection = _.debounce(function () {
-    $scope.$apply(function () {
-      $scope.rosStatus = 'connecting';
-    });
-  }, 2000);
-
-  Hardware.subscribe(function () {
-    $scope.rosStatus = 'ok';
-    checkConnection();
-  });
-
-  // forward all ros events to the current scope
-  ros.forward(['connection', 'error', 'close'], $scope);
-
-  $scope.$on('ros:connection', function () {
-    console.log('ros:connection');
-    $scope.rosStatus = 'connecting';
-  });
-
-  $scope.$on('ros:error', function () {
-    console.log('ros:error');
-    $scope.rosStatus = 'error';
-  });
-
-  $scope.$on('ros:close', function () {
-    console.log('ros:close');
-    $scope.rosStatus = 'closed';
+  robot.on('status', function (status) {
+    $scope.rosStatus = status;
   });
 
 });
