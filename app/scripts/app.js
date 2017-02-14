tue-robotics/dashboard

commit 74bfd6ee755bac678d16269ff7f79ec6726ab688

feat: update nwjs to 0.12.3

app/scripts/app.js
@@ -57,15 +57,34 @@ app.controller('MainCtrl', function ($scope, robot, menu) {
 
     // merge the action icons in only when they are defined
     actions = _.mapValues(actions, function (props, action) {
-        return _.merge(_.clone(props), actionIcons[action]);
+      return _.merge(_.clone(props), actionIcons[action]);
     });
 
     menu.popup(e.x, e.y, actions, function (command) {
       var warning = actions[command].warning;
-      if (warning && !window.confirm(warning)) {
+
+      if (!warning) {
         return;
       }
-      sendCommand(part, command);
+
+      actions = {};
+      actions[warning] = {
+        enabled: false,
+      };
+      actions = _.merge(actions, {
+        Confirm: {
+          icon: 'icons/accepted.png'
+        },
+        Cancel: {
+          icon: 'icons/x3.png'
+        }
+      });
+
+      menu.popup(e.x, e.y, actions, function (confirmResult) {
+        if (confirmResult === 'Confirm') {
+          sendCommand(part, command);
+        }
+      });
     });
   };
 

Gruntfile.js
@@ -59,7 +59,7 @@ module.exports = function (grunt) {
         platforms: ['linux64', 'win32'],
         buildDir: './<%= config.dist %>',
         cacheDir: './.nw-cache',
-        version: '0.10.5',
+        version: '0.12.3',
       },
       src: [
         './app/*',

commit c3377cb06c5b38670473a33a1e0fe4d332276f36

fix(app): Fix missing commands when no confirm was needed
The issue was introduced by 74bfd6ee755bac678d16269ff7f79ec6726ab688

app/scripts/app.js
@@ -63,28 +63,29 @@ app.controller('MainCtrl', function ($scope, robot, menu) {
     menu.popup(e.x, e.y, actions, function (command) {
       var warning = actions[command].warning;
 
-      if (!warning) {
-        return;
+      // build a confirmation dialog
+      if (warning) {
+        actions = {};
+        actions[warning] = {
+          enabled: false,
+        };
+        actions = _.merge(actions, {
+          Confirm: {
+            icon: 'icons/accepted.png'
+          },
+          Cancel: {
+            icon: 'icons/x3.png'
+          }
+        });
+
+        menu.popup(e.x, e.y, actions, function (confirmResult) {
+          if (confirmResult === 'Confirm') {
+            sendCommand(part, command);
+          }
+        });
+      } else {
+        sendCommand(part, command);
       }
-
-      actions = {};
-      actions[warning] = {
-        enabled: false,
-      };
-      actions = _.merge(actions, {
-        Confirm: {
-          icon: 'icons/accepted.png'
-        },
-        Cancel: {
-          icon: 'icons/x3.png'
-        }
-      });
-
-      menu.popup(e.x, e.y, actions, function (confirmResult) {
-        if (confirmResult === 'Confirm') {
-          sendCommand(part, command);
-        }
-      });
     });
   };
 